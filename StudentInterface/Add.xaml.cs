0f446589a8841b8b2cc440bfab30b7e9fddf0ceb
Максим Туркин <maksim.turkin.16@gmail.com>
2023-09-17 13:38:59 +0300

    add interface

---
+++ StudentInterface/Add.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PersonLibrary;
+using StudentInterface;
+using StudentJson;
+
+namespace Student.Interface
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        PersonLibrary.Student student = new PersonLibrary.Student();
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void fin_add_but_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (surname.Text != "" && name.Text != "" && patron.Text != "" && city.Text != "" && pstindex.Text != "" && street.Text != "" && email.Text != "" && number.Text != "" && faculty.Text != "" && specialty.Text != "" && course.Text != "" && group.Text != "")
+            {
+                Json.ReadJson(out List<PersonLibrary.Student> students);
+                student.Fio.Surname = surname.Text;
+                student.Fio.Name = name.Text;
+                student.Fio.Patron = patron.Text;
+                student.Address.City = city.Text;
+                student.Address.PstIndex = pstindex.Text;
+                student.Address.Street = street.Text;
+                student.Contacts.Mail = email.Text;
+                student.Contacts.Phone = number.Text;
+                student.Curriculum.Specialty= specialty.Text;
+                student.Curriculum.Faculty = faculty.Text;
+                student.Curriculum.Course = course.Text;
+                students.Add(student);
+                Json.WriteJson(students);
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+
+            }
+            else
+            {
+               MessageBox.Show("Вы не заполнили все строки!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void close_but(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ StudentInterface/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PersonLibrary;
+using Student.Interface;
+using StudentJson;
+
+namespace StudentInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Json.ReadJson(out List<PersonLibrary.Student> Persons);
+            AllignId(Persons);
+            DataGrid.ItemsSource = Persons;
+        }
+
+       
+        public void AllignId(List<PersonLibrary.Student> Persons) 
+        {
+            int[] capId = new int[Persons.Count];
+            for (int i = 0; i < Persons.Count; i++) 
+            {
+                capId[i] = i;
+                Persons[i].Id = i + 1;
+            }
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Json.ReadJson(out List<PersonLibrary.Student> Persons);
+            AllignId(Persons);
+            if (int.TryParse(LineId.Text, out int number))
+            {
+                Persons.RemoveAll(x => Persons.IndexOf(x) == Convert.ToInt32(LineId.Text) - 1);
+            }
+            else
+            {
+                LineId.Text = string.Empty;
+                MessageBox.Show("Введите номер записи", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            AllignId(Persons);
+            Json.WriteJson(Persons);
+            DataGrid.ItemsSource = Persons;
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Add form2 = new Add();
+            form2.Show();
+            this.Close();
+        }
+    }
+    
+}   
